e37b2ba342ea5346c0613627ed58cee3a4d884d5
Mateusz <wszeborowskimateusz@gmail.com>
2019-11-23 19:12:14 +0100

    Adding full language supports with culture links

---
+++ AppointmentRazor/Utilities/Localization/CurentCultureUtils.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppointmentRazor.Utilities.Localization
+{
+    public static class CurentCultureUtils
+    {
+        public static Dictionary<string, string> GetCurrentCultureRouteData()
+        {
+            var requestCulture = CultureInfo.CurrentCulture;
+            var routeData = new Dictionary<string, string>();
+            routeData["culture"] = requestCulture.Name;
+
+            return routeData;
+        }
+    }
+}
