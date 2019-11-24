a6491552fea2814f76b6da93389d204858530eec
Korneliusz Karbowski <karbowski.korneliusz@gmail.com>
2019-11-24 17:25:38 +0100

    Dodanie UT dla logowania

--- AppointmentApiUT/UserBusinessUT.cs
+++ AppointmentApiUT/UserBusinessUT.cs
@@ -2,6 +2,7 @@ using AppointmentApi.Business;
 using AppointmentApi.DataAccess.Interfaces;
 using AppointmentApi.Tools.Interfaces;
 using AppointmentModel.Model;
+using AppointmentModel.ReturnModel;
 using Moq;
 using System;
 using Xunit;
@@ -11,10 +12,7 @@ namespace AppointmentApiUT
     public class UserBusinessUT
     {
         [Fact]
-        public void AlwaysPass()
-        {
-
-        }
+        public void AlwaysPass() { }
 
         [Theory]
         [InlineData("Login", "Password")]
@@ -40,22 +38,32 @@ namespace AppointmentApiUT
             Assert.NotNull(registeredUser);
         }
 
-        //[Theory]
-        //[InlineData("Login", "Password")]
-        //public void LoginUser(string login, string password)
-        //{
-        //    // Arrange
-        //    var user = new User() { Login = login, Password = password }
-        //    var userDataAccessMock = new Mock<IUserDataAccess>();
-        //    userDataAccessMock.Setup(x => x.GetUser(user.Login))
-        //    var userBusiness = new UserBusiness();
+        [Theory]
+        [InlineData("Login", "Password")]
+        public void LoginUser(string login, string password)
+        {
+            // Arrange
+            var user = new User() { Login = login, Password = password };
 
-        //    // Act
+            var userDataAccessMock = new Mock<IUserDataAccess>();
+            userDataAccessMock.Setup(x => x.GetUser(user.Login)).Returns(new User { Login = login, Password = "hashed" });
 
-        //    // Assert
-        //    Exception ex = Assert
-        //     .Throws<DivideByZeroException>(() => cs.UnsafeDivide(x, y));
+            var hashGeneratorMock = new Mock<IHashGenerator>();
+            hashGeneratorMock.Setup(x => x.GenerateHash(user.Password)).Returns("hashed");
 
-        //}
+            var tokenGeneratorMock = new Mock<ITokenGenerator>();
+            var tokenExpiration = DateTime.Now.AddDays(1);
+            var generatedToken = new Token { Expiration = tokenExpiration, TokenString = "tokenStringValue" };
+            tokenGeneratorMock.Setup(x => x.GenerateToken(It.IsAny<User>())).Returns(new Token { Expiration = generatedToken.Expiration, TokenString = generatedToken.TokenString });
+
+            var userBusiness = new UserBusiness(hashGeneratorMock.Object, userDataAccessMock.Object, tokenGeneratorMock.Object);
+
+            // Act
+            var returnedToken = userBusiness.Login(user);
+
+            // Assert
+            Assert.Equal(generatedToken.Expiration, returnedToken.Expiration);
+            Assert.Equal(generatedToken.TokenString, returnedToken.TokenString);
+        }
     }
 }
