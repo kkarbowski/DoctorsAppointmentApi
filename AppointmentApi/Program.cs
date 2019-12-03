6dfc1d69a0aae8d56919a07c0c0391f8ab1ddd39
Karol <szczepanski.karol0@gmail.com>
2019-12-03 13:26:57 +0100

    Add dependencies for Serilog

--- AppointmentApi/Program.cs
+++ AppointmentApi/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace AppointmentApi
 {
@@ -22,6 +23,8 @@ namespace AppointmentApi
                 {
                     webBuilder.UseKestrel();
                     webBuilder.UseStartup<Startup>();
-                });
+                })
+                .UseSerilog((hostingContext, loggerConfiguration) => loggerConfiguration
+                    .ReadFrom.Configuration(hostingContext.Configuration));
     }
 }
