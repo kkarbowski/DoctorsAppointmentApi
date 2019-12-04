c2e0f7d205b534183d70e5955f4395703896176c
Karol <szczepanski.karol0@gmail.com>
2019-12-04 23:32:14 +0100

    Fix session cookies not working properly with users

--- AppointmentRazor/Startup.cs
+++ AppointmentRazor/Startup.cs
@@ -37,7 +37,10 @@ namespace AppointmentRazor
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSession();
+            services.AddSession(opts =>
+            {
+                opts.Cookie.IsEssential = true;
+            });
 
             services.Configure<CookiePolicyOptions>(options =>
             {
