88667d54adb07a9b47f3a6bd7d323bbd61be4367
Karol <szczepanski.karol0@gmail.com>
2019-12-05 22:38:03 +0100

    Add initial Redis configuration

---
+++ AppointmentApi/Tools/RedisConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppointmentApi.Tools
+{
+    public static class RedisConfiguration
+    {
+        public static string GetRedisIp()
+        {
+            string ip = Environment.GetEnvironmentVariable("REDIS_IP");
+            if (ip == null)
+            {
+                return BASE_IP;
+            }
+            return ip;
+        }
+
+        private const string BASE_IP = "127.0.0.1";
+    }
+}
